21e56a9a51007a8a935205af44d405d1c9a13903
balamir <acimseda2@gmail.com>
2026-03-13 18:29:18 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T15:29:11.756Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-13T15:29:17.623Z
+Commit: Fix memory leak in event handlers
