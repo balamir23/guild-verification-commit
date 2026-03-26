7c2e6fcda2e0da6ffcf5395b9e74eb735ff1788c
balamir <acimseda2@gmail.com>
2026-03-27 02:22:14 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-13T15:29:25.539Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-26T23:22:13.557Z
+Commit: Fix broken links in documentation
